4cc2823c2459d144bcf8adb076b4be2cfeef7285
Nikhil Kumar <1837nik@gmail.com>
2026-04-28 18:19:16 +0530

    fix static file

--- invoice_generator/settings.py
+++ invoice_generator/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES = 'static/'
+STATIC_ROOT = 'static/'
 STATICFILES_DIRS = [
     'invoice_generator/static/',
 ]
