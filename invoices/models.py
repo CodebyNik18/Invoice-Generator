6d8c1045f732000a45ec43b7f3973ba8aa998436
Nikhil Kumar. <1837nik@gmail.com>
2026-05-17 21:40:46 +0530

    add some column in tables

--- invoices/models.py
+++ invoices/models.py
@@ -18,14 +18,25 @@ class Invoice(models.Model):
     to_email = models.EmailField(blank=True, null=True)
     to_address = models.CharField(max_length=300, blank=True, null=True)
 
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
 
 class InvoiceDetails(models.Model):
     invoice = models.OneToOneField(to=Invoice, on_delete=models.CASCADE)
-    pass #'invoice_number': ['INV-2026-001'], 'invoice_date': ['2026-05-17'], 'due_date': ['2026-06-16'], 'currency': ['₹'], 'po_number': ['']
+    invoice_number = models.CharField(max_length=100, blank=True, null=True)
+    invoice_date = models.DateField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    currency = models.CharField(max_length=10, blank=True, null=True)
+    po_number = models.CharField(max_length=50, blank=True, null=True)
 
 class InvoiceItems(models.Model):
     invoice = models.ForeignKey(to=Invoice, on_delete=models.CASCADE)
-    pass #'item_desc': ['', ''], 'item_qty': ['1', '1'], 'item_rate': ['0', '0'], 'item_id': ['1', '2'], 'item_amount': ['0.00', '0.00']
+    item_desc = models.CharField(max_length=300, blank=True, null=True)
+    item_qty = models.CharField(blank=True, null=True)
+    item_rate = models.CharField(blank=True, null=True)
+    item_id = models.CharField(blank=True, null=True)
+    item_amount = models.CharField(blank=True, null=True)
 
 class InvoiceTotal(models.Model):
     invoice = models.OneToOneField(to=Invoice, on_delete=models.CASCADE)
